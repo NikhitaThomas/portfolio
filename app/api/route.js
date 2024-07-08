de7a597d824ce68f2235a1519ecc882b6b478610
Nikita Thomas <nikhitathomas@yahoo.in>
2024-07-08 14:01:10 +1000

    update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,27 +1,27 @@
-import { EmailTemplate } from "../../../components/EmailTemplate";
-import { Resend } from "resend";
+// import { EmailTemplate } from "../../../components/EmailTemplate";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Nikita <nikhitathomas@yahoo.in>",
-      to: ["nikhitathomas@yahoo.in"],
-      subject: "Hello world",
-      react: (
-        <>
-          <p>Email body</p>
-        </>
-      ),
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: "Nikita <nikhitathomas@yahoo.in>",
+//       to: ["nikhitathomas@yahoo.in"],
+//       subject: "Hello world",
+//       react: (
+//         <>
+//           <p>Email body</p>
+//         </>
+//       ),
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
