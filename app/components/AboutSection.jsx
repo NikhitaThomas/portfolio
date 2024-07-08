a3ac12b44f37088ed81684448db123e9ccdf1218
Nikita Thomas <nikhitathomas@yahoo.in>
2024-07-08 14:08:31 +1000

    corrected a few errors

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -61,7 +61,7 @@ function AboutSection() {
   return (
     <section className="text-white my-32" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="" />
         <div className="mt-4 md:mt-0 text-left flex flex-col">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
